361065dff7a4df67f58c2bef80a1a35709c413fe
Syafiq Basri <syafiq@talentx.ai>
2022-05-15 09:27:51 +0800

    Imported bulma and fontawesome in project

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// Main style
+require('@/assets/main.scss');
+
+createApp(App)
+    .use(store)
+    .use(router)
+    .mount('#app')
